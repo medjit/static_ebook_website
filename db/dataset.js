5c669f9bc13130bae3a7dbecfc4ae14397901c41
Medzhit Saitov <saitovmedjit@gmail.com>
2026-05-18 21:43:13 +0300

    Add test playlist data JSON, and load it to the page.

---
+++ db/dataset.js
@@ -0,0 +1,106 @@
+const playlistData = {
+  "folders": [
+    {
+      "folder_name": "jane_doe_mystery",
+      "display_name": "Jane Doe",
+      "files": [
+        {
+          "title": "The Silent Echo",
+          "filename": "silent_echo.mp3",
+          "length": 1840,
+          "size_kb": 4520
+        },
+        {
+          "title": "Midnight in the Garden",
+          "filename": "midnight_garden.mp3",
+          "length": 2105,
+          "size_kb": 51300
+        },
+        {
+          "title": "Shadow of the Pines",
+          "filename": "shadow_pines.mp3",
+          "length": 1920,
+          "size_kb": 48100
+        }
+      ]
+    },
+    {
+      "folder_name": "mark_twain_classics",
+      "display_name": "Mark Twain",
+      "files": [
+        {
+          "title": "Tom Sawyer - Chapter 1",
+          "filename": "tom_sawyer_01.mp3",
+          "length": 600,
+          "size_kb": 12000
+        },
+        {
+          "title": "Tom Sawyer - Chapter 2",
+          "filename": "tom_sawyer_02.mp3",
+          "length": 540,
+          "size_kb": 10800
+        },
+        {
+          "title": "Tom Sawyer - Chapter 3",
+          "filename": "tom_sawyer_03.mp3",
+          "length": 720,
+          "size_kb": 14400
+        },
+        {
+          "title": "Tom Sawyer - Chapter 4",
+          "filename": "tom_sawyer_04.mp3",
+          "length": 615,
+          "size_kb": 12300
+        },
+        {
+          "title": "Tom Sawyer - Chapter 5",
+          "filename": "tom_sawyer_05.mp3",
+          "length": 590,
+          "size_kb": 11800
+        },
+        {
+          "title": "Tom Sawyer - Chapter 6",
+          "filename": "tom_sawyer_06.mp3",
+          "length": 645,
+          "size_kb": 12900
+        },
+        {
+          "title": "Tom Sawyer - Chapter 7",
+          "filename": "tom_sawyer_07.mp3",
+          "length": 680,
+          "size_kb": 13600
+        }
+      ]
+    },
+    {
+      "folder_name": "ivan_vazov_bulgarian",
+      "display_name": "Иван Вазов",
+      "files": [
+        { "title": "Под игото - Част 1", "filename": "pod_igoto_01.mp3", "length": 1200, "size_kb": 24000 },
+        { "title": "Под игото - Част 2", "filename": "pod_igoto_02.mp3", "length": 1150, "size_kb": 23000 },
+        { "title": "Под игото - Част 3", "filename": "pod_igoto_03.mp3", "length": 1300, "size_kb": 26000 },
+        { "title": "Под игото - Част 4", "filename": "pod_igoto_04.mp3", "length": 1100, "size_kb": 22000 },
+        { "title": "Под игото - Част 5", "filename": "pod_igoto_05.mp3", "length": 1250, "size_kb": 25000 },
+        { "title": "Под игото - Част 6", "filename": "pod_igoto_06.mp3", "length": 1400, "size_kb": 28000 },
+        { "title": "Под игото - Част 7", "filename": "pod_igoto_07.mp3", "length": 1080, "size_kb": 21600 },
+        { "title": "Под игото - Част 8", "filename": "pod_igoto_08.mp3", "length": 1120, "size_kb": 22400 },
+        { "title": "Под игото - Част 9", "filename": "pod_igoto_09.mp3", "length": 1190, "size_kb": 23800 },
+        { "title": "Под игото - Част 10", "filename": "pod_igoto_10.mp3", "length": 1310, "size_kb": 26200 },
+        { "title": "Под игото - Част 11", "filename": "pod_igoto_11.mp3", "length": 950, "size_kb": 19000 },
+        { "title": "Под игото - Част 12", "filename": "pod_igoto_12.mp3", "length": 1020, "size_kb": 20400 }
+      ]
+    },
+    {
+      "folder_name": "elena_petrova_poetry",
+      "display_name": "Елена Петрова",
+      "files": [
+        {
+          "title": "Зимни вечери",
+          "filename": "zimni_vecheri.mp3",
+          "length": 315,
+          "size_kb": 6300
+        }
+      ]
+    }
+  ]
+}
